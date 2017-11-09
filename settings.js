5ba2ea76021d84e1808ed6690bdaabea10c06f8a
Joe Grotto <joegrotto@heir.company>
2017-11-08 20:48:17 -0600

    more awaits

--- settings.js
+++ settings.js
@@ -1,10 +1,11 @@
-const config = (module.exports = {});
-config.port = 9050;
-config.host = '127.0.0.1';
-config.player = '--vlc';
-config.remove = true;
-config.blocklist = 'auto';
-config.path = 'tmp';
-config.printmag = false;
-config.health = true;
-config.subtitles = 'none';
+module.exports = {
+  port: 9050,
+  host: '127.0.0.1',
+  player: '--vlc',
+  remove: true,
+  blocklist: 'auto',
+  path: 'tmp',
+  printmag: false,
+  health: true,
+  subtitles: 'none'
+};
